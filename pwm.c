b17409ee5db337fecedccc65a1f2ccf10260b356
Alex Papazoglou <papazoga@gmail.com>
2017-03-08 21:52:32 -0800

    Improved channel switching and cleaned up PWM code.

--- pwm.c
+++ pwm.c
@@ -3,7 +3,7 @@
 
 extern unsigned char sintab[N_ENTRIES];
 
-static unsigned char pwm_side;
+static unsigned char pwm_curchan;
 static unsigned char pwm_on;
 static int pwm_position;
 
@@ -12,13 +12,7 @@ unsigned char pwm_amplitude;
 
 #define INITIAL_AMPLITUDE 50
 #define INITIAL_TICKS     132  /* ((F_CPU / (7680 * N_ENTRIES))-1) */
-
-enum {
-	TOP_OFF = 0,
-	TOP_ON,
-	BOTTOM_OFF,
-	BOTTOM_ON,
-};
+#define INITIAL_DEADTIME  2    /* 1us deadtime */
 
 void pwm_init()
 {
@@ -47,43 +41,55 @@ void pwm_init()
 
 	TCCR2A = (1<<WGM21);		/* Clear on compare match */
 	TCCR2B = 3;			/* Prescale by 32. We are CPU limited. */
-	OCR2A = INITIAL_TICKS;		/* 16MHz/4 = 4MHz */
+	OCR2A = INITIAL_TICKS;
 	TIMSK2 = (1<<OCIE2A);	        /* Interrupt when match occurs */
 
-	pwm_side = 0;
+	pwm_curchan = 0;
 	pwm_on = 0;
 	pwm_position = 0;
 	pwm_amplitude = INITIAL_AMPLITUDE;
-	pwm_deadtime = 1;
-
-	TCCR0A =  (1<<COM0A1)                 /* Clear OC0A on compare match going up, set on match going down */
-		| (1<<COM0B0) | (1<<COM0B1)   /* Set OC0B on compare match going up, clear on match going down */
-		| (1<<WGM00);	              /* Phase-correct PWM 0x00 to 0xff */
-	TCCR0B = 2;
-
-	TCCR1A =  (1<<COM1A1)                 /* Clear OC1A on compare match going up, set on match going down */
-		| (1<<COM1B0) | (1<<COM1B1)   /* Set OC0B on compare match going up, clear on match going down */
-		| (1<<WGM10);                 /* Phase-correct (8-bit) PWM 0x00 to 0xff */
-	TCCR1B = 2;
+	pwm_deadtime = INITIAL_DEADTIME;
 }
 
-#if 0
-void pwm_disable()
+void enable_channel(int ch)
 {
-	PORTD &= ~((1<<3) | (1<<5) | (1<<6));
-	PORTB &= ~((1<<1) | (1<<2) | (1<<3));
-
-	TCCR0A = 0;
-	TCCR1A = 0;
-
-	TCCR0B = 0;
-	TCCR1B = 0;
-
-	TIMSK0 &= ~1;
+	switch (ch) {
+	case 0:
+		TCCR0A =  (1<<COM0A1)                 /* Clear OC0A on compare match going up, set on match going down */
+			| (1<<COM0B0) | (1<<COM0B1)   /* Set OC0B on compare match going up, clear on match going down */
+			| (1<<WGM00);	              /* Phase-correct PWM 0x00 to 0xff */
+		TCCR0B = 2;
+		TCNT0 = 0;
+		break;
+	case 1:
+		TCCR1A =  (1<<COM1A1)                 /* Clear OC1A on compare match going up, set on match going down */
+			| (1<<COM1B0) | (1<<COM1B1)   /* Set OC0B on compare match going up, clear on match going down */
+			| (1<<WGM10);                 /* Phase-correct (8-bit) PWM 0x00 to 0xff */
+		TCCR1B = 2;
+		TCNT1 = 0;
+		break;
+	default:
+		break;
+	}
 }
-#endif
 
-#define PWM_CUTOFF 10
+void disable_channel(int ch)
+{
+	switch (ch) {
+	case 0:
+		TCCR0B = 0;
+		PORTD |= (1<<6);
+		PORTD &= ~(1<<5);
+		break;
+	case 1:
+		TCCR1B = 0;
+		PORTB |= (1<<1);
+		PORTB &= ~(1<<2);
+		break;
+	default:
+		break;
+	}
+}
 
 void pwm_update()
 {
@@ -92,25 +98,17 @@ void pwm_update()
 
 	if (pwm_position == 0) {
 		PORTD ^= (1<<7);
-		pwm_side ^= 1;
+		pwm_curchan ^= 1;
 
-		TCNT0 = 0;
-		TCNT1 = 0;
-		if (pwm_side) {
-			DDRD &= ~((1<<5) | (1<<6));
-			DDRB |= (1<<1) | (1<<2);
-		} else {
-			DDRD |= (1<<5) | (1<<6);
-			DDRB &= !((1<<1) | (1<<2));
-		}
-	
+		enable_channel(pwm_curchan);	
+		disable_channel(pwm_curchan^1);
 	}
 
-	OCR0A = val + pwm_deadtime;
-	OCR0B = val;
+	OCR0A = val;
+	OCR0B = val + pwm_deadtime;
 
-	OCR1A = val + pwm_deadtime;
-	OCR1B = val;
+	OCR1A = val;
+	OCR1B = val + pwm_deadtime;
 
 out:
 	pwm_position = (pwm_position+1) % N_ENTRIES;
